d8fae5a4ba3db30a98b50acc03bb06cac00a03f3
precious4life <preciousorugiengha@gmail.com>
2024-03-05 19:29:43 +0100

    wdd230 site first dev

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+const userName = "Precious Orugiengha";
+const userCountry = "Nigeria";
+document.querySelector(".name").innerHTML = userName;
+document.querySelector(".country").innerHTML =  userCountry;
+
+const currentYear = new Date().getFullYear();
+document.querySelector("#year").innerHTML = currentYear;
