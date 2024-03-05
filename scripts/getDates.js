bbde10f7712c9432525b9caf78c00fb710eb570b
precious4life <preciousorugiengha@gmail.com>
2024-03-05 20:18:57 +0100

    checking javascript dates

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,5 +3,12 @@ const userCountry = "Nigeria";
 document.querySelector(".name").innerHTML = userName;
 document.querySelector(".country").innerHTML =  userCountry;
 
-const currentYear = new Date().getFullYear();
-document.querySelector("#year").innerHTML = currentYear;
+const currentDate = document.querySelector("#year");
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+currentDate.innerHTML = new Date().toLocaleDateString("en-GB", options);
