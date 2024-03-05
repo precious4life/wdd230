7178662d12c3b29a164525197fbee705c3fe19c7
precious4life <preciousorugiengha@gmail.com>
2024-03-05 20:45:33 +0100

    .js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,12 +3,6 @@ const userCountry = "Nigeria";
 document.querySelector(".name").innerHTML = userName;
 document.querySelector(".country").innerHTML =  userCountry;
 
-const currentDate = document.querySelector("#year");
-const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-};
 
-currentDate.innerHTML = new Date().toLocaleDateString("en-GB", options);
+
+
